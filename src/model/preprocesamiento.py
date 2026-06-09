d8f94ef1995997e2744d4b67b69f42cc54e4326f
Luis Méndez <lgma9677@gmail.com>
2026-06-09 15:54:53 -0600

    Feat: preprocesamiento.py. Clase con funciones que nos ayudarán al preprocesamiento de los datos del dataframe

---
+++ src/model/preprocesamiento.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import(OneHotEncoder, StandardScaler)
+
+def cargar_preparar_datos(ruta):
+    '''
+    Carga un archivo csv que contiene los datos del proyecto
+
+    Args:
+        ruta : la ruta del archivo CSV a cargar
+    '''
+    df = pd.read_csv('../src/data/INM_estatal_dic25_clean.csv')
+
+    return df
+
+def separar_features_target(df):
+    '''
+    Separa el dataframe en el conjunto de features (variables que no son la objetivo)
+    y la variable objetivo.
+
+    Aplica una transformación logaritmica para tratar el desvalance observado en el EDA
+    de la variable objetivo
+
+    Args:
+        df : el dataframe que tiene tanto features como la variable objetivo
+
+    Returns:
+        X : el dataframe sin la columna de la variable objetivo
+        y : La columna de la variable objetivo (ya con la transformación)
+    '''
+
+    target = "incidencia_delictiva"
+
+    y = np.log1p(df[target])
+    X = df.drop(columns=[target])
+
+    return X, y
+
+def construir_preprocesador(X):
+    categoricas = X.select_dtypes(
+        include=["object", "category"]
+    ).columns.tolist()
+
+    numericas = X.select_dtypes(
+        include=["int64", "float64"]
+    ).columns.tolist()
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "cat", OneHotEncoder(
+                    handle_unknown="ignore"
+                ),
+                categoricas
+            ),
+            (
+                "num",
+                StandardScaler(),
+                numericas
+            )
+        ]
+    )
+
+    return preprocessor
